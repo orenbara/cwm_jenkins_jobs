fe3202989dea3e8362c7bcd90c8b9ff6bc84a95f
root <root@OREN-ASUS.localdomain>
2023-09-28 11:47:05 +0300

    added cpu pytest code 5

--- almalinux9/func_testing.py
+++ almalinux9/func_testing.py
@@ -1,60 +1,42 @@
 import os
 import requests
+import pytest
 
-class FuncTesting:
-
-    def __init__(self, server_id, auth_client_id,auth_client_secret, cwm_url):
+class TestFuncTesting:
+    
+    @pytest.fixture(autouse=True)
+    def setup_class(self):
+        self.server_id = os.environ.get('serverId')
+        self.auth_client_id = os.environ.get('clientId')
+        self.auth_client_secret = os.environ.get('secret')
+        self.cwm_url = os.environ.get('cwm_url')
         self.cwm_headers = {
             "content-type": "application/json",
-            "AuthClientId": auth_client_id,
-            "AuthSecret": auth_client_secret
+            "AuthClientId": self.auth_client_id,
+            "AuthSecret": self.auth_client_secret
         }
-        self.server_id = server_id
-        self.cwm_url = cwm_url
-    
+
+    """
     def test_cwm_auth(self):
-        # Fetching values from Jenkins environment variables
-        client_id = os.environ.get('clientId')
-        secret_key = os.environ.get('secret')
-
         url = "https://staging.cloudwm.com/service/authenticate"
-
         payload = {
-            "clientId": client_id,
-            "secret": secret_key
+            "clientId": self.auth_client_id,
+            "secret": self.auth_client_secret
         }
-
         response = requests.request("POST", url, headers=self.cwm_headers, json=payload)
         print(response.text)
-    
+        
+        # Add any assertions related to auth test here
+    """
+
     def test_cwm_cpu(self):
         url = f"https://{self.cwm_url}/service/server/{self.server_id}/cpu"
-
-        payload = "{\"cpu\":\"2B\"}"
-        
+        payload = "{\"cpu\":\"4B\"}"
         response = requests.request("PUT", url, headers=self.cwm_headers, data=payload)
-        
         print(response.text)
-        return response
-        
-def test_cwm_functions():
-    # variables:
-    server_id = os.environ.get('serverId')
-    auth_client_id = os.environ.get('clientId')
-    auth_client_secret = os.environ.get('secret')
-    cwm_url = os.environ.get('cwm_url')
 
-    tester = FuncTesting(server_id,auth_client_id ,auth_client_secret, cwm_url)
-    
-    # Authenticate test to CWM:        
-    #tester.test_cwm_auth()
-    
-    
-    ################ Test CPU ################
-    response = tester.test_cwm_cpu()
-    
-    # Check the HTTP status code
-    assert 200 <= response.status_code < 300, f"Expected success status code, got {response.status_code}"
-    response_content = response.json()
-    if isinstance(response_content, dict):  # Check if the response is a dictionary
-        assert "errors" not in response_content, f"Found errors in response: {response_content['errors']}"
+        assert 200 <= response.status_code < 300, f"Expected success status code, got {response.status_code}"
+        
+        response_content = response.json()
+        if isinstance(response_content, dict):  # Check if the response is a dictionary
+            assert "errors" not in response_content, f"Found errors in response: {response_content['errors']}"
